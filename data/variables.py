785e521d2853e612ab4c279972ebf334de29ebc6
hhampson <hannah.hampson96@gmail.com>
2021-02-25 16:19:42 -0800

    add desired start year and end year for our period of analysis

--- data/variables.py
+++ data/variables.py
@@ -5,3 +5,6 @@ dry_month_start = 5  # start of "dry" season of prediction beginning of May
 dry_month_end = 10  # end of "dry" season of prediction end of October
 wet_month_start = dry_month_end + 1  # start of "wet" season of inputs beginning of November
 wet_month_end = dry_month_start - 1  # end of "wet" season of inputs end of April
+
+year_start = 2015
+year_end = 2020
