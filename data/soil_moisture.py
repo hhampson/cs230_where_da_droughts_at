3e5312e6e28e8d37b66be4b896e401ded7564bd1
bennettbolen <79602785+bennettbolen@users.noreply.github.com>
2021-03-05 16:09:19 -0700

    Changed output to match standard
    This script will compile previously downloaded data into a final soil moisture dataset that is of the correct form to be input into build_dataset.py

--- data/soil_moisture.py
+++ data/soil_moisture.py
@@ -1,32 +1,81 @@
 import numpy as np
 import h5py
+import os
 from os import listdir
 from os.path import isfile, join
-from data.variables import *
 
 
-def main():
-    filenames = list_files()
+def build_sm_array():
+    # Builds the soil moisture dtaset from the individual data files. The .h5 files should be located in the folder
+    # specified by rootdir, with data from each year in a separate subfolder.
 
-    full_data, averaged_data, lats, lons = combine_arrays(filenames)
+    rootdir = 'C:/Users/Bennett/Documents/School/Stanford/4. CS230 Project/Test Data/'
+    idx = 0
+    dirs = os.listdir(rootdir)
+    # path = rootdir + dirs[0]
+    # print(listdir(path))
+    #
+    # h5_files = [f for f in listdir(path) if f.endswith(".h5")]
+    # print(h5_files)
+    for idx in range(len(dirs)):
 
-    global trimmed_data_SM, trimmed_lats_SM, trimmed_lons_SM
-    trimmed_data_SM, trimmed_lats_SM, trimmed_lons_SM = trim_data(lat_lims, lon_lims, averaged_data, lats, lons)
+        if idx == 0:
+            trimmed_data, trimmed_lats, trimmed_lons = extract_one_year(rootdir + dirs[idx])
+            print(trimmed_data.shape)
+        else:
+            trimmed_data_temp, trimmed_lats_temp, trimmed_lons_temp = extract_one_year(rootdir + dirs[idx])
+            trimmed_data = np.concatenate((trimmed_data, trimmed_data_temp))
+
+            trimmed_lats = np.concatenate((trimmed_lats, trimmed_lats_temp), axis=1)
+            trimmed_lons = np.concatenate((trimmed_lons, trimmed_lons_temp))
+
+            if np.sum(trimmed_lats[:, idx-1] != trimmed_lats[:, idx]):
+                raise Exception("Latitudes don't match. File Number: " + str(idx+1))
+
+            if np.sum(trimmed_lons[idx-1, :] != trimmed_lons[idx, :]):
+                raise Exception("Longitudes don't match. File Number" + str(idx+1))
+        idx += 1
+    return trimmed_data, trimmed_lats, trimmed_lons
 
 
-def list_files(path='.'):
-    h5_files = [f for f in listdir(path) if (isfile(f) and f.endswith(".h5"))]
+def extract_one_year(path):
+    # Extracts all the .h5 files in one folder and combines them to an averaged output for that year
+    filenames = list_h5_files(path)
+
+    full_data, averaged_data, lats, lons = combine_arrays(filenames, path)
+
+    lat_lims = [42, 31]  # latitude range over California
+    lon_lims = [-125, -114]  # longitude range over California
+
+    trimmed_data, trimmed_lats, trimmed_lons = trim_data(lat_lims, lon_lims, averaged_data, lats, lons)
+
+    trimmed_data = np.reshape(trimmed_data, (1, trimmed_data.shape[0], trimmed_data.shape[1]))
+    trimmed_lats = np.reshape(trimmed_lats, (trimmed_lats.shape[0], 1))
+    trimmed_lons = np.reshape(trimmed_lons, (1, trimmed_lons.shape[0]))
+
+    return trimmed_data, trimmed_lats, trimmed_lons
+
+
+def list_h5_files(path='.'):
+    # Returns a list of all .h5 files in the directory specified by path
+
+    h5_files = [f for f in listdir(path) if f.endswith(".h5")]
 
     return h5_files
 
 
-def combine_arrays(filenames):
+def combine_arrays(filenames, path):
+    # Combines the data of all the files stored in the list filenames into numpy arrays. Outputs are a combined array
+    # which includes each individual data file, and an averaged array which is an average of all the files. Input
+    # arrays must be of the same dimensions
+
     for i in range(len(filenames)):
+
         if i == 0:
-            [combined_data, lats, lons] = import_file(filenames[i])
+            [combined_data, lats, lons] = import_file(path + "/" + filenames[i])
 
         else:
-            [surface, lats_temp, lons_temp] = import_file(filenames[i])
+            [surface, lats_temp, lons_temp] = import_file(path + "/" + filenames[i])
             combined_data = np.concatenate((combined_data, surface))
             lats = np.concatenate((lats, lats_temp), axis=1)
             lons = np.concatenate((lons, lons_temp))
@@ -37,13 +86,14 @@ def combine_arrays(filenames):
             if np.sum(lons[i-1, :] != lons[i, :]):
                 raise Exception("Longitudes don't match. File Number" + str(i+1))
 
-    averaged_data = np.mean(combined_data, axis=0)
+    averaged_data = np.average(combined_data, weights=(combined_data < 100), axis=0)
     averaged_data = np.reshape(averaged_data, (1, averaged_data.shape[0], averaged_data.shape[1]))
 
     return combined_data, averaged_data, lats[:, 0], lons[0, :]
 
 
 def import_file(filename):
+    # Imports a .h5 file and extracts the surface level soil moisture data, the latitudes, and the longitudes
     with h5py.File(filename, "r") as f:
         surface = f["Forecast_Data"]["sm_surface_forecast"][:, :]
         surface = np.reshape(surface, (1, surface.shape[0], surface.shape[1]))
@@ -57,9 +107,10 @@ def import_file(filename):
         return surface, lats, lons
 
 
-def trim_data(area, lats, lons):
-    lat_coords, lat_idx = get_area_coords(lats, lat_lims)
-    long_coords, lon_idx = get_area_coords(lons, lon_lims)
+def trim_data(lat_lim, lon_lim, area, lats, lons):
+    # Trims data arrays to the specified latitude and longitude limits
+    lat_coords, lat_idx = get_area_coords(lats, lat_lim)
+    long_coords, lon_idx = get_area_coords(lons, lon_lim)
 
     trimmed_data = area[0, lat_idx[0]:lat_idx[1], lon_idx[0]:lon_idx[1]]
     trimmed_lats = lats[lat_idx[0]:lat_idx[1]]
@@ -81,5 +132,6 @@ def get_area_coords(coordinate_list, lims):
     return area_coords, idx
 
 
-if __name__ == '__main__':
-    main()
+SIX_MONTH_VALUES, LAT, LON = build_sm_array()
+
+
