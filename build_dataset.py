3935097a651df1e75606182bde231d3c2f0af3f7
hhampson <hannah.hampson96@gmail.com>
2021-03-14 22:09:03 -0700

    output X and Y numpy arrays (v2) for the many to one RNN model. adds in processing of soil moisture, accounts for nans in training examples, and now inputs data from saved numpy arrays in git

--- build_dataset.py
+++ build_dataset.py
@@ -6,70 +6,77 @@ This script is for building the dataset.
 """
 
 import numpy as np
-from data import precipitation, drought_index, temperature, soil_moisture
+# from data import precipitation, drought_index, temperature, soil_moisture
 from tempfile import TemporaryFile
 import pandas as pd
-# from mpl_toolkits.basemap import Basemap
-from matplotlib import pyplot as plt
 from data.variables import *  # imports variables such as lat_lims, lon_lims
 
 
 # Extract data from drought index script
-DI_VALUES = drought_index.SIX_MONTH_VALUES
-DI_YEARS = drought_index.YEARS
-DI_LAT = drought_index.LAT
-DI_LON = drought_index.LON
+DI_VALUES = np.load('processed_data/SIX_MONTH_VALUES_DI.npy')
+DI_LAT = np.load('processed_data/LAT_DI.npy')
+DI_LON = np.load('processed_data/LON_DI.npy')
 
 # Specify number of input variables
 NUM_VARS = 4  # precipitation, max temp, min temp, and soil moisture
 
 
 def main():
+    # If drought index matrix 4 years, then cut off last irrelevant year
+    if DI_VALUES.shape[0] > 3:
+        di = DI_VALUES[0:3, :, :]
+    else:
+        di = DI_VALUES
+
     # Extract data from precipitation script
-    precipitation_values_large = precipitation.SIX_MONTH_VALUES
-    precipitation_years = precipitation.YEARS
-    precipitation_lat = precipitation.LAT
-    precipitation_lon = precipitation.LON
+    precipitation_values_large = np.load('processed_data/MONTHLY_PRECIP_V2.npy')[:, ::-1, :]  # flip latitude for precip
+    precipitation_lat = np.load('processed_data/LAT_PRECIP.npy')[::-1]
+    precipitation_lon = np.load('processed_data/LON_PRECIP.npy')
 
     # Extract data from temperature script, does not need to be downsampled
-    min_temp = temperature.SIX_MONTH_VALUES_TMIN
-    max_temp = temperature.SIX_MONTH_VALUES_TMAX
-    temperature_years = temperature.YEARS
-    temperature_lat = temperature.LAT
-    temperature_lon = temperature.LON
+    min_temp = np.load('processed_data/MONTHLY_MINTEMP_V2.npy')
+    max_temp = np.load('processed_data/MONTHLY_MAXTEMP_V2.npy')
+    temperature_lat = np.load('processed_data/LAT_TEMP.npy')
+    temperature_lon = np.load('processed_data/LON_TEMP.npy')
 
     # Extract data from soil moisture script
-    sm_values_large = soil_moisture.SIX_MONTH_VALUES
-    sm_years = [2015, 2016, 2017]
-    sm_lat = soil_moisture.LAT
-    sm_lon = soil_moisture.LON
-
-    assert len(precipitation_years) == len(temperature_years) == len(sm_years) == len(DI_YEARS)
+    sm_values_large = concat_sm()
+    sm_lat = np.load('processed_data/LAT_SMAP_2015.npy')
+    sm_lon = np.load('processed_data/LON_SMAP_2015.npy')
 
     # Downsample input variables to fit drought index resolution
-    precip = downsample(precipitation_lat, precipitation_lon, precipitation_values_large, precipitation_years, 'sum')
-    sm = downsample(sm_lat, sm_lon, sm_values_large, sm_years, 'avg')
+    precip = downsample(precipitation_lat, precipitation_lon, precipitation_values_large, 'sum')
+    sm = downsample(sm_lat, sm_lon, sm_values_large, 'avg')
 
-    # TODO: sanity check by plotting map of values
+    assert precip.shape == min_temp.shape == max_temp.shape == sm.shape
 
-    y_nans = build_y(DI_VALUES)
-    x_nans = build_x(precip, max_temp, min_temp, sm)
+    x, y = build_x_y(di, precip, min_temp, max_temp, sm)
 
-    # Drop any examples that have NaN values
-    x, y = drop_nans(x_nans, y_nans)
     return y, x
 
 
-def downsample(var_lat, var_lon, var_values_large, var_years, method):
+def concat_sm():
     """
-    Downsamples data spatially to align with drought index scale. Designed to ignore nans.
+    Concatenate various soil moisture arrays into one.
+    """
+    sm_2015 = np.load('processed_data/MONTHLY_VALUES_SMAP_2015.npy')
+    sm_2016 = np.load('processed_data/MONTHLY_VALUES_SMAP_2016.npy')
+    sm_2017 = np.load('processed_data/MONTHLY_VALUES_SMAP_2017.npy')
+    sm = np.concatenate((sm_2015, sm_2016, sm_2017), axis=0)
+    return sm
+
+
+def downsample(var_lat, var_lon, var_values_large, method):
+    """
+    Downsamples data spatially to align with drought index scale. Ignores nans by using
+    nansum and nanmean.
 
     Inputs:
         Method: either 'sum' or 'avg' the values being downsampled
     """
     grid_size = DI_LAT[0] - DI_LAT[1]
-    values_array = np.zeros((len(var_years), len(DI_LAT), len(DI_LON)))
-    for year_idx in range(len(var_years)):
+    values_array = np.zeros((var_values_large.shape[0], len(DI_LAT), len(DI_LON)))
+    for time in range(var_values_large.shape[0] - 1):
         for lat_idx in range(len(DI_LAT)):
             for lon_idx in range(len(DI_LON)):
                 _, fine_lat_idx = get_area_coords(var_lat, [(DI_LAT[lat_idx] - 0.5 * grid_size),
@@ -77,10 +84,12 @@ def downsample(var_lat, var_lon, var_values_large, var_years, method):
                 _, fine_lon_idx = get_area_coords(var_lon, [(DI_LON[lon_idx] - 0.5 * grid_size),
                                                             (DI_LON[lon_idx] + 0.5 * grid_size)])
                 if method == 'sum':
-                    value = var_values_large[fine_lat_idx[1]:fine_lat_idx[0], fine_lon_idx[0]:fine_lon_idx[1]].nansum()
-                if method == 'avg':
-                    value = np.nanmean(var_values_large[fine_lat_idx[1]:fine_lat_idx[0], fine_lon_idx[0]:fine_lon_idx[1]], axis=None)
-                values_array[year_idx, lat_idx, lon_idx] = float(value)
+                    value = np.nansum(var_values_large[time, fine_lat_idx[0]:fine_lat_idx[1],
+                                      fine_lon_idx[1]:fine_lon_idx[0]], axis=None)
+                else:  # method = 'avg'
+                    value = np.nanmean(var_values_large[time, fine_lat_idx[0]:fine_lat_idx[1],
+                                       fine_lon_idx[1]:fine_lon_idx[0]], axis=None)
+                values_array[time, lat_idx, lon_idx] = float(value)
     return values_array
 
 
@@ -101,41 +110,10 @@ def get_area_coords(coordinate_list, lims):
     return area_coords, idx
 
 
-def plot_map(variable, lon, lat, title):
-    # plot variable on map over California
-    # specify map lat/lon limits
-    lon_min = int(lon_lims[0])
-    lat_min = lat_lims[1]
-    lon_max = int(lon_lims[1])
-    lat_max = lat_lims[0]
-
-    map = Basemap(projection='merc', llcrnrlon=lon_min, llcrnrlat=lat_min, urcrnrlon=lon_max, urcrnrlat=lat_max,
-                  resolution='l')  # projection, lat/lon extents and resolution of polygons to draw
-    lons, lats = np.meshgrid(lon, lat)
-    x, y = map(lons, lats)
-
-    # add states and border masks
-    plt.figure(figsize=(15, 10))
-    map.drawcoastlines()
-    map.drawstates()
-    map.drawcountries()
-
-    # add lat/lon masks
-    parallels = np.arange(lat_min, lat_max, 5.)  # make latitude lines ever 5 degrees from 30N-50N
-    meridians = np.arange(lon_min, lon_max, 5.)  # make longitude lines every 5 degrees from 95W to 70W
-    map.drawparallels(parallels, labels=[1, 0, 0, 0], fontsize=10)
-    map.drawmeridians(meridians, labels=[0, 0, 0, 1], fontsize=10)
-
-    # plot data
-    z = map.contourf(x, y, variable)
-    cb = map.colorbar(z, "right", size="5%", pad="2%")
-    plt.title(title)
-    cb.set_label('Value')
-    plt.show()
-
-
 def build_y(di_values):
     """
+    **Note: Used for linear regression model but not many to one RNN.
+
     Build y array from drought index array to input into model.
     Each lat, lon, during one year, corresponds to one training example.
     """
@@ -145,11 +123,15 @@ def build_y(di_values):
         new_vals = np.reshape(di_values[year_idx, :, :], (1, di_values.shape[1] * di_values.shape[2]))
         y[year_idx] = new_vals
     y = np.concatenate([y[year_idx] for year_idx in range(0, di_values.shape[0])], axis=1)
-    assert y.shape == (1, m)
+    y = y.T
+    assert y.shape == (m, 1)
     return y
 
 
 def build_x(precip, max_temp, min_temp, sm):
+    """
+    **Note: Used for linear regression model but not many to one RNN.
+    """
     m = DI_VALUES.shape[0] * DI_VALUES.shape[1] * DI_VALUES.shape[2]  # number of training examples
     x = {}
     grid_area = DI_VALUES.shape[1] * DI_VALUES.shape[2]
@@ -166,8 +148,38 @@ def build_x(precip, max_temp, min_temp, sm):
     return x
 
 
-def drop_nans(x_nans, y_nans):
+def build_x_y(di, precip, min_temp, max_temp, sm):
     """
+    Builds the x and y arrays to be used in time series model. Takes out any training examples
+    with NaN in any of the inputs or y matrix. Creates array by going through every lat lon
+    value at each corresponding year.
+    """
+    m = 0
+    x = np.empty((0, 6, NUM_VARS))
+    y = np.empty((0, 1))
+    for year_idx in range(di.shape[0] - 1):
+        for lat_idx in range(di.shape[1]):
+            for lon_idx in range(di.shape[2]):
+                x_m = np.zeros((1, 6, NUM_VARS))
+                x_m[:, :, 0] = precip[6 * year_idx:6 * (year_idx + 1), lat_idx, lon_idx]
+                x_m[:, :, 1] = min_temp[6 * year_idx:6 * (year_idx + 1), lat_idx, lon_idx]
+                x_m[:, :, 2] = max_temp[6 * year_idx:6 * (year_idx + 1), lat_idx, lon_idx]
+                x_m[:, :, 3] = sm[6 * year_idx:6 * (year_idx + 1), lat_idx, lon_idx]
+                y_m = di[year_idx, lat_idx, lon_idx].reshape(1, 1)
+                if not np.isnan(x_m).any():  # no nans in x matrix
+                    if not np.isnan(y_m):  # no nans in y matrix
+                        m += 1
+                        x = np.concatenate((x, x_m), axis=0)
+                        y = np.concatenate((y, y_m), axis=0)
+    print("Number of training examples: " + str(m))
+    assert x.shape[0] == y.shape[0] == m
+    return x, y
+
+
+def drop_nans_old(x_nans, y_nans):
+    """
+    **Note: Used for linear regression model but not many to one RNN.
+
     Take in X and Y matrices and throws out training examples with NaN values in them.
     Does so by creating a pandas dataframe from X and Y matrices then using
     dropna().
@@ -187,7 +199,6 @@ def drop_nans(x_nans, y_nans):
 
 
 Y, X = main()
-Y = np.array(Y)
 outfile = TemporaryFile()
 # np.save('Y',Y)  # first test
 # np.save('X',X)  # first test
