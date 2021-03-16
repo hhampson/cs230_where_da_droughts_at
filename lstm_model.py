a5b987b5cefb8c71cf69cc75d5e1d2d7b8bbadac
bennettbolen <79602785+bennettbolen@users.noreply.github.com>
2021-03-15 19:31:38 -0600

    added prediction and test set evaluation

--- lstm_model.py
+++ lstm_model.py
@@ -14,6 +14,7 @@ hidden_layers = 32
 learning_rate = 0.001
 epochs = 150
 
+
 # MSE Loss
 # Adam Optimizer
 
@@ -36,6 +37,7 @@ class DroughtDataset(Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.Y[idx][0]
 
+
 ## loading training data
 drought_dataset = DroughtDataset('x_train.npy', 'y_train.npy')
 
@@ -45,6 +47,8 @@ for i in range(len(drought_dataset)):
 
 ## call dataloader
 dataloader = DataLoader(drought_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+
 # batch,seq,input_var
 
 ## create LSTM model
@@ -53,23 +57,26 @@ class LSTM(nn.Module):
         super().__init__()
         self.hidden_layer_size = hidden_layer_size
 
-        self.lstm = nn.LSTM(input_size, hidden_layer_size,batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
 
         self.linear = nn.Linear(hidden_layer_size, output_size)
 
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
-                            torch.zeros(1,1,self.hidden_layer_size))
+        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
+                            torch.zeros(1, 1, self.hidden_layer_size))
 
     def forward(self, input_seq):
-        lstm_out, hidden_cell = self.lstm(input_seq)#, self.hidden_cell.cuda())
+        lstm_out, hidden_cell = self.lstm(input_seq)  # , self.hidden_cell.cuda())
         predictions = self.linear(lstm_out[:, -1])
         return predictions.squeeze(1)
+
 
 ## cross entropy loss and optimizer
 model = LSTM()
 model = model
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+model.train()
 
 ## training the model
 for i in trange(epochs):
@@ -78,17 +85,17 @@ for i in trange(epochs):
         seq, labels = batch
         seq, labels = seq.float(), labels.float()
         optimizer.zero_grad()
-        #model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
+        # model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
         #                torch.zeros(1, 1, model.hidden_layer_size))
         y_pred = model(seq)
         single_loss = loss_function(y_pred, labels)
-        
+
         single_loss.backward()
         optimizer.step()
         sum_loss += single_loss
-        
-    if i%5 == 1:
-        print(f'epoch: {i:3} loss: {sum_loss.cpu().item()/len(dataloader):10.8f}')
+
+    if i % 5 == 1:
+        print(f'epoch: {i:3} loss: {sum_loss.cpu().item() / len(dataloader):10.8f}')
 
 print(f'epoch: {i:3} loss: {single_loss.cpu().item():10.10f}')
 
@@ -103,5 +110,21 @@ for i in range(len(drought_dataset_test)):
     # sample[1] is the y test
 
 ## making predictions
+print(drought_dataset_test.X.shape)
+test_inputs = drought_dataset_test.X[-(test_size-1):].tolist()
+#print(len(test_inputs))
+model.eval()
 
+#print(drought_dataset_test.X)
+for i in range(test_size):
+    inputs = torch.FloatTensor(drought_dataset_test.X)
+    with torch.no_grad():
+        model.hidden = (torch.zeros(1, 1, model.hidden_layer_size),
+                        torch.zeros(1, 1, model.hidden_layer_size))
+        out_data = model(inputs)
 
+predicted_y = np.reshape(out_data.numpy(), (out_data.numpy().shape[0], 1))
+actual_y = drought_dataset_test.Y
+
+test_error = np.mean(np.square((predicted_y-actual_y)))
+print(test_error)
